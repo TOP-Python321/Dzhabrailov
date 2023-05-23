2f46da186f821729ddd048a8c6f6f96fc73746eb
Gennadiy <renardes@gmail.com>
2023-05-23 09:36:26 +0500

    2023.04.16: комментарии, правки, итоги

--- 2023.04.16/4.py
+++ 2023.04.16/4.py
@@ -5,6 +5,7 @@ sum_1 = ord(coordinate_1[0]) + int(coordinate_1[1])
 sum_2 = ord(coordinate_2[0]) + int(coordinate_2[1])
 sum_coord = sum_1 + sum_2
 
+# КОММЕНТАРИЙ: уж при копировании-то чужого кода можно хотя бы ошибок не допускать?
 if 'a' <= coordinate_1[0] <= 'h'
         and 'a' <= coordinate_2[0] <= 'h'
         
@@ -15,7 +16,11 @@ if 'a' <= coordinate_1[0] <= 'h'
     else:
         print('Нет')
 
+
+# КОММЕНТАРИЙ: вы не запускали этот код, и не могли получить эти значения
 # a1
 # b2
-    
 # да
+
+
+# ИТОГ: переделать — 0/5
